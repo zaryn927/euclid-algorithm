d8d82e1fca6d3cc62f59c1eec47b7b53e741d225
Sky Link <s.z_link@yahoo.com>
2017-06-14 08:58:10 -0600

    Euclid foundation. Parses command line integers.

---
+++ src/edu/cnm/deepdive/euclid/Gcd.java
@@ -0,0 +1,11 @@
+package edu.cnm.deepdive.euclid;
+
+public class Gcd {
+
+  public static void main(String[] args) {
+    int a = Integer.parseInt(args[0]);
+    int b = Integer.parseInt(args[1]);
+    System.out.println(a + " " + b);
+  }
+
+}
