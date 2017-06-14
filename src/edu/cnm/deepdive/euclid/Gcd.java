0fde9064bcd523631221df77b2cd7f80077940c8
Sky Link <s.z_link@yahoo.com>
2017-06-14 09:42:09 -0600

    Exception handling and usage

--- src/edu/cnm/deepdive/euclid/Gcd.java
+++ src/edu/cnm/deepdive/euclid/Gcd.java
@@ -1,11 +1,28 @@
 package edu.cnm.deepdive.euclid;
 
 public class Gcd {
+  
+  public static final String USAGE = "Usage:%n"
+      + "\tjava [options] edu.cnm.deepdive.euclid.Gcd [a b]%n" 
+      + "%n" 
+      + "where%n"
+      + "\toptions = VM options%n" 
+      + "\t[a b] = two integers for GCD calculation%n";
+  
+  private static void emitUsage() {
+    System.out.printf(USAGE);
+  }
 
   public static void main(String[] args) {
-    int a = Integer.parseInt(args[0]);
-    int b = Integer.parseInt(args[1]);
-    System.out.println(a + " " + b);
+    try {
+      int a = Integer.parseInt(args[0]);
+      int b = Integer.parseInt(args[1]);
+      System.out.println(a + " " + b);
+    } catch (NumberFormatException ex) {
+      ex.printStackTrace();
+      emitUsage();
+    }
+
   }
 
 }
